021ebf96ba62a4d394950cb1cfcb0226abef3715
Emir Marques <emirdeliz@gmail.com>
2018-09-13 10:12:20 -0300

    Update dictionary

--- admin/screens/widget/view/input/pt/index.js
+++ admin/screens/widget/view/input/pt/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Deseja confirmar o envio dos dados',
+  THE_FILE_ARE_LARGER_THAN_1_MB: 'O(s) arquivo(s) $0 são maiores que 1MB e não serão enviados para TagoIO',
   THESE_EMAIL_ARE_INVALID: 'Esses e-mail(s) são inválido(s)',
 };
