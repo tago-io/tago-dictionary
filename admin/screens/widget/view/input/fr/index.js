3754864526e12ab15fb0231d225373f734c9c226
Emir Marques <emirdeliz@gmail.com>
2018-09-13 14:39:32 -0300

    Update dictionary

--- admin/screens/widget/view/input/fr/index.js
+++ admin/screens/widget/view/input/fr/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Confirmez-vous la soumission du formulaire',
-  THE_FILE_ARE_LARGER_THAN_1_MB: 'Le(s) fichier(s) $0 sont supérieurs à 1 Mo et ne seront pas envoyés à TagoIO',
+  THE_FILE_ARE_LARGER_THAN_1_MB: 'Le(s) fichier(s) $0 sont supérieurs à 1 MB et ne seront pas envoyés à TagoIO',
   THESE_EMAIL_ARE_INVALID: 'Ces email(s) sont invalide(s)',
 };
