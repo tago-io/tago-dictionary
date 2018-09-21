26341ec58b01a076c8e1a7d4799bf54325cad434
Emir Marques <emirdeliz@gmail.com>
2018-09-21 17:14:40 -0300

    Update dictionary

--- admin/screens/widget/view/input/fr/index.js
+++ admin/screens/widget/view/input/fr/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Confirmez-vous la soumission du formulaire',
-  THE_FILE_ARE_LARGER_THAN_1_MB: 'Le(s) fichier(s) $0 sont supérieurs à 1 MB et ne seront pas envoyés à TagoIO',
+  THE_FILE_ARE_LARGER_THAN: 'Vous avez atteint la limite (0$) pour cette demande. Les fichiers $1 ne seront pas téléchargés sur TagoIO.',
   THESE_EMAIL_ARE_INVALID: 'Ces email(s) sont invalide(s)',
 };
--- admin/screens/widget/view/input/pt/index.js
+++ admin/screens/widget/view/input/pt/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Deseja confirmar o envio dos dados',
-  THE_FILE_ARE_LARGER_THAN_1_MB: 'O(s) arquivo(s) $0 são maiores que 1MB e não serão enviados para TagoIO',
+  THE_FILE_ARE_LARGER_THAN: 'Você atingiu o limite ($0) para esta solicitação. Os arquivos $1 não serão enviados para o TagoIO.',
   THESE_EMAIL_ARE_INVALID: 'Esses e-mail(s) são inválido(s)',
 };
