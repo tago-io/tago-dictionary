bb39580f7d6852b3205034e80d42825ffc2bf317
Emir Marques <emirdeliz@gmail.com>
2018-09-24 11:13:03 -0300

    Update dictionary

--- admin/screens/widget/view/input/en/index.js
+++ admin/screens/widget/view/input/en/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Do you confirm the form submission',
-  THE_FILE_ARE_LARGER_THAN: 'You have reached the limit ($0) for this request. The files $1 will not be uploaded to TagoIO.',
+  THE_FILE_ARE_LARGER_THAN_OLD: 'You have reached the limit ($0) for this request. The files $1 will not be uploaded to TagoIO.',
+  THE_FILE_ARE_LARGER_THAN: 'The file(s) $0 are larger than 1 MB, and will not be sent to TagoIO',
   THESE_EMAIL_ARE_INVALID: 'These email(s) are invalid(s)',
 };
--- admin/screens/widget/view/input/fr/index.js
+++ admin/screens/widget/view/input/fr/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Confirmez-vous la soumission du formulaire',
-  THE_FILE_ARE_LARGER_THAN: 'Vous avez atteint la limite (0$) pour cette demande. Les fichiers $1 ne seront pas téléchargés sur TagoIO.',
+  THE_FILE_ARE_LARGER_THAN_OLD: 'Vous avez atteint la limite (0$) pour cette demande. Les fichiers $1 ne seront pas téléchargés sur TagoIO.',
+  THE_FILE_ARE_LARGER_THAN: 'Le(s) fichier(s) $0 sont supérieurs à 1 MB et ne seront pas envoyés à TagoIO',
   THESE_EMAIL_ARE_INVALID: 'Ces email(s) sont invalide(s)',
 };
