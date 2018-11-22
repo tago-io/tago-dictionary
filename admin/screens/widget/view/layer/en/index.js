7baede6127d446e002d05e7f55de5e3db547ea5e
Emir Marques <emirdeliz@gmail.com>
2018-11-22 14:29:04 -0200

    Updated dictionary

--- admin/screens/widget/view/layer/en/index.js
+++ admin/screens/widget/view/layer/en/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   EDIT_MARKER: 'Edit Marker',
   ENTER_WITH_LABEL_MARKER: 'enter with the label of marker',
+  ENTER_WITH_IMAGE_MARKER: 'enter with the url of image',
   ICON_MARKER: 'Icon of marker',
   LABEL_MARKER: 'Label of marker',
+  IMAGE_MARKER: 'Image of marker',
 };
