83a64c2d158a473e3fd9f851f62008031ba99642
Emir Marques <emirdeliz@gmail.com>
2019-08-15 15:23:22 -0300

    refactor: update the message to file limit on Form

--- admin/screens/widget/view/input/es/index.js
+++ admin/screens/widget/view/input/es/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: '¿Confirmas el envío del formulario',
   THE_FILE_ARE_LARGER_THAN_OLD: 'Has alcanzado el límite ($0) para esta solicitud. Los archivos $1 no se subirán a TagoIO.',
-  THE_FILE_ARE_LARGER_THAN: 'El archivo(s) $0 tiene más de 1 MB y no se enviará a TagoIO',
+  THE_FILE_ARE_LARGER_THAN: 'El archivo(s) $0 tiene más de 3 MB y no se enviará a TagoIO',
   THESE_EMAIL_ARE_INVALID: 'Esto(s) correo(s) electrónico(s) son inválido(s)',
 };
