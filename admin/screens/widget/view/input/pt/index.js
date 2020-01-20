3b4308f6781a5f8318c8ab72c309cab95d4f42da
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2020-01-20 10:58:03 -0300

    feat: changed 3mb to 20mb in file translation

--- admin/screens/widget/view/input/pt/index.js
+++ admin/screens/widget/view/input/pt/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   YOU_CONFIRM_FORM_SUBMISSION: 'Deseja confirmar o envio dos dados',
   THE_FILE_ARE_LARGER_THAN_OLD: 'Você atingiu o limite ($0) para esta solicitação. Os arquivos $1 não serão enviados para o TagoIO.',
-  THE_FILE_ARE_LARGER_THAN: 'O(s) arquivo(s) $0 são maiores que 3MB e não serão enviados para TagoIO',
+  THE_FILE_ARE_LARGER_THAN: 'O(s) arquivo(s) $0 são maiores que 20MB e não serão enviados para TagoIO',
   THESE_EMAIL_ARE_INVALID: 'Esses e-mail(s) são inválido(s)',
 };
