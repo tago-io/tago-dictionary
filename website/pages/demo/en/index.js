0cabbf46bc4867bd8da359ee09e60e7b2f5a766d
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2020-05-27 13:44:39 -0300

    feat: new translate to demo page

---
+++ website/pages/demo/en/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  DEMO_TITLE: 'Demo',
+  DEMO_DESCRIPTION: 'Request your personalized one-to-one demo to learn how to start building your own IoT platform for free.',
+  DEMO_HEADER_TITLE: 'Demo',
+  DEMO_HEADER_SUBTITLE: 'Get a Free Demo of TagoIO\'s IoT Plataform',
+
+  DEMO_SECTION_TITLE: 'Request demo',
+  DEMO_FIRST_PARAGRAPH: 'See for yourself how TagoIO can be used to deploy your entire connected product solution in no-time, all while generating the best return on investment.',
+
+  DEMO_LIST_HEADER: 'By running your applications on TagoIO, you will be able to:',
+  DEMO_LIST_ITEM1:  'Connect to hundreds of different types of IoT devices',
+  DEMO_LIST_ITEM2:  'Build dashboards, reports, and automate processes',
+  DEMO_LIST_ITEM3:  'Add analytics that creates exceptional value for your customers',
+  DEMO_LIST_ITEM4:  'Integrate with an unlimited number of webservices',
+  DEMO_LIST_ITEM5:  'Deploy a scalable and fully-customized solution for end-users',
+  DEMO_LIST_ITEM6:  'Engage with customers through email, chats, and more!',
+  DEMO_LIST_FOOTER:  '... all from one place.',
+};
---
+++ website/pages/demo/pt/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  DEMO_TITLE: 'Demo',
+  DEMO_DESCRIPTION: 'Solicite uma demonstração personalizada para aprender como iniciar o desenvolvimento da sua própria plataforma IoT gratuitamente.',
+  DEMO_HEADER_TITLE: 'Demo',
+  DEMO_HEADER_SUBTITLE: 'Obtenha uma demonstração da Plataforma de IoT TagoIO gratuitamente',
+
+  DEMO_SECTION_TITLE: 'Solicite uma demo',
+  DEMO_FIRST_PARAGRAPH: 'Veja você mesmo como a TagoIO pode ser usada para implantar toda a sua solução de produtos conectados rapidamente, enquanto gera o melhor retorno de investimento.',
+
+  DEMO_LIST_HEADER: 'Executando suas aplicações na TagoIO, você poderá:',
+  DEMO_LIST_ITEM1:  'Conectado a centenas de diferentes tipos de dispositivos IoT',
+  DEMO_LIST_ITEM2:  'Crie painéis, relatórios, e automatize processos',
+  DEMO_LIST_ITEM3:  'Adicione análises que criem valor excepcional para os seus clientes',
+  DEMO_LIST_ITEM4:  'Integre com um número ilimitado de webservices',
+  DEMO_LIST_ITEM5:  'Publique uma solução escalável e totalmente costumizada para usuários finais',
+  DEMO_LIST_ITEM6:  'Interaja com seus clientes por meio de e-mails, chats, e mais!',
+  DEMO_LIST_FOOTER:  '... Tudo de um único lugar.',
+};
