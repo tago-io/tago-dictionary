6b45a2539db63a3ecf39b751c00eea61c38fdff3
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2021-03-16 13:33:38 -0300

    feat: add fr to mobile-run

---
+++ mobile-run/fr/index.js
@@ -0,0 +1,44 @@
+module.exports = {
+  MOBILE_COMMUM_NEXT: "Continuez",
+  MOBILE_COMMUM_CANCEL_SETUP: "Annuler la configuration",
+  MOBILE_COMMUM_DONE: "Fait",
+
+  MOBILE_BTN_SIGNIN: "Session initiale",
+  MOBILE_BTN_SIGNUP: "Créer un compte",
+
+  MOBILE_SECTION_NOTIFICATION: "Notifications",
+  MOBILE_SECTION_SETTINGS: "Réglages",
+
+  MOBILE_SETTINGS_ARESURE: "Tu es sûr?",
+  MOBILE_SETTINGS_DORELLY: "Voulez-vous vraiment sortir?",
+  MOBILE_SETTINGS_KEEPAWAKE: "Reste éveillé",
+  MOBILE_SETTINGS_LOGOUT: "Fermer la session",
+  MOBILE_SETTINGS_NOTIFICATIONS: "Notifications",
+  MOBILE_SETTINGS_SHOWLASTUPDATE: "Afficher l'heure de la dernière mise à jour",
+  MOBILE_SETTINGS_SWITCH_ACCOUNTS: "Changer de compte",
+
+  MOBILE_RUN_ENTER_DOMAIN: "Entrez le domaine TagoRun",
+  MOBILE_RUN_NEW_TO_RUN: "Nouveau sur TagoRun?",
+  MOBILE_RUN_LEARN_MORE: "En savoir plus",
+  MOBILE_RUN_SLIDE_1_TITLE: "Bienvenue",
+  MOBILE_RUN_SLIDE_1_MSG: "Le meilleur moyen d'accéder aux applications IoT où que vous soyez",
+
+  MOBILE_RUN_DASHBOARD_SETTINGS: "Paramètres du tableau de bord",
+  MOBILE_RUN_DEVICE_SETTINGS: "Configuration de l'appareil",
+  MOBILE_RUN_GENERAL_SETTINGS: "Configuration générale",
+  MOBILE_RUN_IM_CONNECTED: "Je suis connecté",
+  MOBILE_RUN_NEW_DEVICE: "Configurer un nouvel appareil",
+
+  MOBILE_WIFI_SETUP_DESC: `Si vous voyez une notification indiquant "Le Wi-Fi n'a pas accès à Internet. Appuyez pour afficher les options", appuyez dessus et sélectionnez "Oui".`,
+  MOBILE_WIFI_SETUP_DEVICEFOUND: "Appareil trouvé!",
+  MOBILE_WIFI_SETUP_LOOKING: "Recherche d'un appareil",
+  MOBILE_WIFI_SETUP_RETRIEVING: "Récupération des paramètres",
+
+  MOBILE_ACCOUNTS: "Compte",
+  MOBILE_ADD_ACCOUNT: "Ajouter un compte",
+
+  MOBILE_FIELDS_MISSING: "Il manque des champs",
+
+  MOBILE_ACCESS_DENIED: "Autorisation refusée",
+};
+
